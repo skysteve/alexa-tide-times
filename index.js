dd33eafa2b5a43e9165aa0fea2de35de571d85f7
Steve Jenkins <steve@skysteve.com>
2017-03-06 21:07:14 +0000

    remove src/index in favour of just moving to the high level

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 /**
  * Created by steve on 06/03/2017.
  */
+const Alexa = require('alexa-sdk');
+const handlers = require('./src/handlers');
 
-exports = require('./src/index');
+exports.handler = (event, context/* , callback*/) => {
+  const alexa = Alexa.handler(event, context);
+  alexa.appId = process.env.ALEXA_SKILL_ID;
+
+  alexa.registerHandlers(handlers);
+  alexa.execute();
+};
