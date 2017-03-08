86d3156a20ad60a303abfad3b8c1cf4e1b3dd572
Steve Jenkins <steve@skysteve.com>
2017-03-08 21:07:06 +0000

    store favourite location (hopefully)

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ exports.handler = (event, context/* , callback*/) => {
   const alexa = Alexa.handler(event, context);
   alexa.appId = process.env.ALEXA_SKILL_ID;
 
+  alexa.dynamoDBTableName = 'AlexaTideTimesSkill';
   alexa.registerHandlers(handlers);
   alexa.execute();
 };
--- src/handlers.js
+++ src/handlers.js
@@ -3,8 +3,12 @@
 const locationManager = require('./locationManager');
 const tideManger = require('./tideManager');
 
-function getLocation(intent) {
-  return locationManager.getNearestMatch(intent.slots.Location.value);
+const FAV_LOCATION_KEY = 'favourite_location';
+const UNKNOWN_LOC_REPLY = 'Sorry, I didn\'t hear a location and you haven\'t set a favourite yet. You can set a favourite by saying "Alexa ask tide times to set my favourite location as Place"';
+const CARD_TITLE = 'Tide Times';
+
+function getLocation(intent, returnDefault, favLoc) {
+  return locationManager.getNearestMatch(intent.slots.Location.value, returnDefault) || favLoc;
 }
 
 function formatTimes(arrTimes) {
@@ -13,10 +17,14 @@ function formatTimes(arrTimes) {
 
 module.exports = {
   LaunchRequest() {
-    this.emit(':tellWithCard', 'Welcome to tide times', 'Tide Times', 'See http://www.ukho.gov.uk/easytide/EasyTide/ShowPrediction.aspx');
+    this.emit(':tellWithCard', 'Welcome to tide times', CARD_TITLE, 'See http://www.ukho.gov.uk/easytide/EasyTide/ShowPrediction.aspx');
   },
   BothTimes() {
-    const location = getLocation(this.event.request.intent);
+    const location = getLocation(this.event.request.intent, true, this.attributes[FAV_LOCATION_KEY]);
+
+    if (!location) {
+      return this.emit(':tellWithCard', UNKNOWN_LOC_REPLY, CARD_TITLE, UNKNOWN_LOC_REPLY);
+    }
 
     tideManger.getTideTimes(location)
       .then((result) => {
@@ -43,11 +51,15 @@ module.exports = {
       })
       .catch((ex) => {
         console.error(ex);
-        this.emit(':tellWithCard', `Failed to load tide times for ${location}`, 'Tide Times', `Failed to load tide times for "${location}"`);
+        this.emit(':tellWithCard', `Failed to load tide times for ${location}`, CARD_TITLE, `Failed to load tide times for "${location}"`);
       });
   },
   HighTide() {
-    const location = getLocation(this.event.request.intent);
+    const location = getLocation(this.event.request.intent, true, this.attributes[FAV_LOCATION_KEY]);
+
+    if (!location) {
+      return this.emit(':tellWithCard', UNKNOWN_LOC_REPLY, CARD_TITLE, UNKNOWN_LOC_REPLY);
+    }
 
     tideManger.getTideTimes(location)
       .then((result) => {
@@ -63,11 +75,15 @@ module.exports = {
       })
       .catch((ex) => {
         console.error(ex);
-        this.emit(':tellWithCard', `Failed to load tide times for ${location}`, 'Tide Times', `Failed to load tide times for "${location}"`);
+        this.emit(':tellWithCard', `Failed to load tide times for ${location}`, CARD_TITLE, `Failed to load tide times for "${location}"`);
       });
   },
   LowTide() {
-    const location = getLocation(this.event.request.intent);
+    const location = getLocation(this.event.request.intent, true, this.attributes[FAV_LOCATION_KEY]);
+
+    if (!location) {
+      return this.emit(':tellWithCard', UNKNOWN_LOC_REPLY, CARD_TITLE, UNKNOWN_LOC_REPLY);
+    }
 
     tideManger.getTideTimes(location)
       .then((result) => {
@@ -83,8 +99,19 @@ module.exports = {
       })
       .catch((ex) => {
         console.error(ex);
-        this.emit(':tellWithCard', `Failed to load tide times for ${location}`, 'Tide Times', `Failed to load tide times for "${location}"`);
+        this.emit(':tellWithCard', `Failed to load tide times for ${location}`, CARD_TITLE, `Failed to load tide times for "${location}"`);
       });
+  },
+  SaveFavourite() {
+    const location = getLocation(this.event.request.intent, false);
+
+    if (!location) {
+      return this.emit(':tell', `Sorry I couldn't find tide time for ${location}`);
+    }
+
+    this.attributes[FAV_LOCATION_KEY] = location;
+    this.emit(':saveState', true);
+    this.emit(':tell', `Your favourite location has been saved as ${location}`);
   },
   Unhandled() {
     this.emit(':tell', 'Sorry, I failed to understand your query, please try again');
